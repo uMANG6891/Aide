a869a05d19d199d7b9716409079312f3f3f3a5be
Umang Pandya <umang6891@gmail.com>
2016-11-19 11:58:23 +0530

    - Fixed checked box infinite loop

--- app/src/main/java/com/umangpandya/aide/ui/adapter/NotesAdapter.java
+++ app/src/main/java/com/umangpandya/aide/ui/adapter/NotesAdapter.java
@@ -2,21 +2,17 @@ package com.umangpandya.aide.ui.adapter;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.database.Cursor;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.umangpandya.aide.R;
 import com.umangpandya.aide.data.provider.NotesContract.NoteEntry;
-import com.umangpandya.aide.data.storage.AccountManager;
 import com.umangpandya.aide.utility.Constants;
 import com.umangpandya.aide.utility.Debug;
 
@@ -84,66 +80,22 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    updateCheckedStatus();
-                }
-            });
-            view.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                            context,
-                            android.R.layout.simple_selectable_list_item);
-                    arrayAdapter.add("Delete");
-                    AlertDialog.Builder builderSingle = new AlertDialog.Builder(context);
-                    builderSingle.setAdapter(
-                            arrayAdapter,
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(final DialogInterface dialog, int which) {
-                                    switch (which) {
-                                        // Delete
-                                        case 0:
-                                            AlertDialog.Builder builderInner = new AlertDialog.Builder(context);
-                                            builderInner
-                                                    .setMessage(R.string.delete_note_message)
-                                                    .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
-                                                        @Override
-                                                        public void onClick(DialogInterface dialog, int which) {
-                                                            context.getContentResolver().delete(
-                                                                    NoteEntry.CONTENT_URI,
-                                                                    NoteEntry._ID + " = ?",
-                                                                    new String[]{String.valueOf(DATA.getInt(Constants.COL_NOTE_ID))}
-                                                            );
-                                                        }
-                                                    })
-                                                    .setNegativeButton(R.string.dismiss, new DialogInterface.OnClickListener() {
-                                                        @Override
-                                                        public void onClick(DialogInterface dialogInterface, int i) {
-                                                            dialogInterface.dismiss();
-                                                        }
-                                                    })
-                                                    .show();
-                                            break;
-
-                                    }
-                                }
-                            });
-                    builderSingle.show();
-                    return true;
+                    DATA.moveToPosition(position);
+                    int checked = DATA.getInt(Constants.COL_NOTE_CHECKED);
+                    checked = ++checked % 2;
+                    cbChecked.setChecked(checked != 0);
                 }
             });
             cbChecked.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    updateCheckedStatus();
+                    updateCheckedStatus(b ? 1 : 0);
                 }
             });
         }
 
-        private void updateCheckedStatus() {
+        private void updateCheckedStatus(int checked) {
             DATA.moveToPosition(position);
-            int checked = DATA.getInt(Constants.COL_NOTE_CHECKED);
-            checked = ++checked % 2;
             ContentValues cv = new ContentValues();
             cv.put(NoteEntry.COLUMN_CHECKED, checked);
             context.getContentResolver().update(
